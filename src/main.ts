7e5260cc43892ae73f678e25245897889d660a7f
mihinov <kamahinmihail@gmail.com>
2022-12-27 14:55:20 +0300

    добавил красоты

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import gsap from 'gsap';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
@@ -12,7 +13,8 @@ const sizes = {
 const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
-	color: '#00ff83'
+	color: '#00ff83',
+	roughness: 0.5
 });
 const mesh = new THREE.Mesh(geometry, material);
 const light = new THREE.PointLight(0xffffff, 1, 100);
@@ -26,12 +28,13 @@ const renderer = new THREE.WebGLRenderer({canvas: canvasNode});
 const controls = new OrbitControls(camera, canvasNode);
 
 light.position.set(0, 10, 10);
+light.intensity = 1.25;
 camera.position.z = 20;
 scene.add(mesh);
 scene.add(light);
 scene.add(camera);
 controls.enableDamping = true;
-controls.dampingFactor = 0.01;
+controls.dampingFactor = 1;
 controls.enablePan = false;
 controls.enableZoom = false;
 controls.autoRotate = true;
@@ -59,10 +62,36 @@ const loop = () => {
 
 loop();
 
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 } , { z: 1, x: 1, y: 1 });
+tl.fromTo('nav', { y: '-100%'}, { y: '0%' });
+tl.fromTo('.title', { opacity: 0 }, { opacity: 1 });
 
 
 
+let mouseDown = false;
+let rgb = [];
 
+window.addEventListener('mousedown', () => mouseDown = true);
+window.addEventListener('mouseup', () => mouseDown = false);
+
+window.addEventListener('mousemove', (e) => {
+	if (mouseDown === true) {
+		rgb = [
+			Math.round((e.pageX / sizes.width) * 255),
+			Math.round((e.pageY / sizes.height) * 255),
+			150
+		]
+
+		const newColor = new THREE.Color(`rgb(${rgb.join(', ')})`);
+		gsap.to(mesh.material.color, {
+			r: newColor.r,
+			g: newColor.g,
+			b: newColor.b 
+		});
+	}
+});
 
 
 
